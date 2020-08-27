b178b31b3253177a8db60e628df85c6898d929ed
Eleonore Fournier-Tombs <eleonore@Eleonores-MacBook-Pro.local>
2020-08-27 09:58:06 -0400

    Added comments to script

--- ethiopia/convert_fewsnet_to_admin2.py
+++ ethiopia/convert_fewsnet_to_admin2.py
@@ -1,8 +1,25 @@
-#import
+"""
+This script takes the FEWSNET IPC shapefiles provided by on fews.net and overlays them with an admin2 shapefile, in order
+to provide an IPC value for each admin2 district. In the case where there are multiple values per district, the IPC value
+with the maximum area is selected.
+
+The configurations are set up inside the script. For usability, they should eventually be set up in a config file or
+passed externally.
+
+In FEWSNET IPC, there are 3 possible categories of maps - 'CS' (Current State), 'ML1' (3 months projection), 'ML2' (6 months projection).
+Any one of these is compatible with the script.
+
+Possible IPC values range from 1 (least severe) to 5 (most severe, famine).
+
+"""
+
+# Imports
 
 import geopandas as gpd
 import pandas as pd
 from datetime import date
+
+# Configurations
 
 DATES = ['200907','200910','201001','201004','201007','201010','201101', '201104', '201107', '201110',
        '201201', '201204', '201207', '201210', '201301', '201304', '201307', '201310', '201401', '201404', '201407',
@@ -18,13 +35,21 @@ COUNTRY = 'ethiopia'
 
 regions = {'west-africa': 'WA', 'east-africa': 'EA'}
 
+# Functions
+
 def return_max_cs(date, admin2, df, STATUS):
+    '''
+    Return the maximum IPC value in an admin2 district.
+    '''
     sub = df.loc[(df['date']==date)&(df['ADMIN2']==admin2)]
     mx = sub['area'].max()
     row = sub[['date', 'ADMIN0', 'ADMIN1', 'ADMIN2', STATUS]].loc[sub['area']==mx]
     return row
 
 def shapefiles_to_df():
+    '''
+    Compile the shapefiles to a dataframe
+    '''
     df = gpd.GeoDataFrame()
     for d in DATES:
         shape = PATH+REGION + d + '/'+regions[REGION] +'_'+ d + '_' + STATUS +'.shp'
@@ -34,6 +59,9 @@ def shapefiles_to_df():
     return df
 
 def merge_admin2(shape_df):
+    '''
+    Merge the geographic boundary information shapefile with the IPC shapefile.
+    '''
     admin2 = gpd.read_file(ADMIN2_SHP)
     overlap = gpd.overlay(admin2, shape_df, how='intersection')
     overlap = overlap.drop_duplicates()
@@ -45,6 +73,10 @@ def merge_admin2(shape_df):
 
 
 def main():
+    '''
+    Import the FEWSNET IPC maps, select the appropriate geographic boundary, select the max IPC category.
+    Saves a CSV to file and returns the dataframe.
+    '''
     shape = shapefiles_to_df()
     overlap = merge_admin2(shape)
     new_df = pd.DataFrame(columns=['date', 'ADMIN0', 'ADMIN1', 'ADMIN2', STATUS])
