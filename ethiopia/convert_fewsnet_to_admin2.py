8308854dbc509c2d65fa5e292868d85942d1bf2a
Tinkaa <tinkavalentijn@gmail.com>
2020-09-08 18:13:55 +0200

    refactor code such that configs in main

--- ethiopia/convert_fewsnet_to_admin2.py
+++ ethiopia/convert_fewsnet_to_admin2.py
@@ -19,73 +19,73 @@ import geopandas as gpd
 import pandas as pd
 from datetime import date
 
-# Configurations
 
-DATES = ['200907','200910','201001','201004','201007','201010','201101', '201104', '201107', '201110',
-       '201201', '201204', '201207', '201210', '201301', '201304', '201307', '201310', '201401', '201404', '201407',
-       '201410', '201501', '201504', '201507', '201510', '201602', '201606','201610',
-       '201706', '201710', '201802', '201806', '201810', '201812','201902', '201906', '201910']
 
-PATH = '../../FEWSNET/East Africa/EA_proj/'
-STATUS = 'ML2' # choice of CS, ML1, ML2
-ADMIN2_SHP = 'ET_Admin2_2014/ET_Admin2_2014.shp'
-REGION = 'east-africa'
-today = date.today()
-COUNTRY = 'ethiopia'
 
-regions = {'west-africa': 'WA', 'east-africa': 'EA'}
-
-# Functions
-
-def return_max_cs(date, admin2, df, STATUS):
-    '''
-    Return the maximum IPC value in an admin2 district.
-    '''
-    sub = df.loc[(df['date']==date)&(df['ADMIN2']==admin2)]
-    mx = sub['area'].max()
-    row = sub[['date', 'ADMIN0', 'ADMIN1', 'ADMIN2', STATUS]].loc[sub['area']==mx]
-    return row
-
-def shapefiles_to_df():
+def shapefiles_to_df(path,status,dates,region="East-Africa",regionabb="EA"):
     '''
     Compile the shapefiles to a dataframe
     '''
     df = gpd.GeoDataFrame()
-    for d in DATES:
-        shape = PATH+REGION + d + '/'+regions[REGION] +'_'+ d + '_' + STATUS +'.shp'
+    for d in dates:
+        shape = path+region + d + '/'+ regionabb +'_'+ d + '_' + status +'.shp'
         gdf = gpd.read_file(shape)
         gdf['date'] = pd.to_datetime(d, format='%Y%m')
         df = df.append(gdf, ignore_index=True)
     return df
 
-def merge_admin2(shape_df):
+def merge_admin2(df,path_admin,status,adm0c,adm1c,adm2c):
     '''
     Merge the geographic boundary information shapefile with the IPC shapefile.
     '''
-    admin2 = gpd.read_file(ADMIN2_SHP)
-    overlap = gpd.overlay(admin2, shape_df, how='intersection')
+    admin2 = gpd.read_file(path_admin)
+    admin2 = admin2[[adm0c,adm1c,adm2c,'geometry']]
+    overlap = gpd.overlay(admin2, df, how='intersection')
     overlap = overlap.drop_duplicates()
     overlap['area'] = overlap['geometry'].area
-    columns = ['ADMIN0', 'ADMIN1', 'ADMIN2', 'date', STATUS, 'geometry', 'area']
+    columns = [adm0c,adm1c,adm2c,status, 'date', 'geometry', 'area']
     overlap = overlap[columns]
     return overlap
 
+def return_max_cs(date, admin2, df, status,adm0c,adm1c,adm2c):
+    '''
+    Return the maximum IPC value in an admin2 district.
+    '''
+    sub = df.loc[(df['date']==date)&(df[adm2c]==admin2)]
+    mx = sub['area'].max()
+    row = sub[['date', adm0c, adm1c, adm2c,status]].loc[sub['area']==mx]
+    return row
 
+def gen_csml1m2(ipc_path,bound_path,status,dates,adm0c="ADM0_EN",adm1c="ADM1_EN",adm2c="ADM2_EN"):
+    df_ipc = shapefiles_to_df(ipc_path,status,dates)
+    overlap = merge_admin2(df_ipc,bound_path,status,adm0c,adm1c,adm2c)
+    new_df = pd.DataFrame(columns=['date',status,adm0c,adm1c,adm2c])
+    for d in overlap['date'].unique():
+        for a in overlap[adm2c].unique():
+            row = return_max_cs(d, a, overlap,status,adm0c,adm1c,adm2c)
+            new_df = new_df.append(row)
+    return new_df
 
 def main():
     '''
     Import the FEWSNET IPC maps, select the appropriate geographic boundary, select the max IPC category.
     Saves a CSV to file and returns the dataframe.
     '''
-    shape = shapefiles_to_df()
-    overlap = merge_admin2(shape)
-    new_df = pd.DataFrame(columns=['date', 'ADMIN0', 'ADMIN1', 'ADMIN2', STATUS])
-    for d in overlap['date'].unique():
-        for a in overlap['ADMIN2'].unique():
-            row = return_max_cs(d, a, overlap, STATUS)
-            new_df = new_df.append(row)
-    new_df.to_csv(COUNTRY + '_admin2_fewsnet_' + STATUS + str(today) + '.csv')
-    return new_df
+   
+    DATES = ['200907','200910','201001','201004','201007','201010','201101', '201104', '201107', '201110',
+       '201201', '201204', '201207', '201210', '201301', '201304', '201307', '201310', '201401', '201404', '201407',
+       '201410', '201501', '201504', '201507', '201510', '201602', '201606','201610',
+       '201706', '201710', '201802', '201806', '201810', '201812','201902', '201906', '201910']
+    STATUS_LIST=["CS","ML1","ML2"]
+    COUNTRY = 'ethiopia'
+    PATH = 'Data/EA_FewsNET/'
+    ADMIN2_SHP = "Data/ET_Admin2_OCHA_2019/eth_admbnda_adm2_csa_bofed_20190827.shp" #'ET_Admin2_2014/ET_Admin2_2014.shp'
+
+    today = date.today()
+    for STATUS in STATUS_LIST:
+        df=gen_csml1m2(PATH,ADMIN2_SHP,STATUS,DATES)
+        df.to_csv("Data/" + COUNTRY + '_admin2_fewsnet_' + df.date.min().strftime('%Y%m%d') +'_'+ df.date.max().strftime('%Y%m%d')+'_'+STATUS+ '.csv')
+
 
 if __name__=='__main__':
     main()
