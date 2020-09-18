fd311646db4d0ccaf5030eeacbd3c93a893ab093
Tinkaa <tinkavalentijn@gmail.com>
2020-09-18 17:49:33 +0200

    Add docstrings

--- convert_fewsnet_to_admin2.py
+++ convert_fewsnet_to_admin2.py
@@ -24,6 +24,15 @@ import numpy as np
 def shapefiles_to_df(path, status, dates, region, regionabb):
     """
     Compile the shapefiles to a dataframe
+    Args:
+        path: path to directory that contains the FewsNet shapefiles
+        status: type of FewsNet prediction: CS (current), ML1 (near-term projection) or ML2 (medium-term projection)
+        dates: list of dates for which FewsNet data should be included. The files of these dates have to be present in the directory (path)!
+        region: region that the fewsnet data covers, e.g. "east-africa"
+        regionabb: abbreviation of the region that the fewsnet data covers, e.g. "EA"
+
+    Returns:
+        df: DataFrame that contains all the shapefiles of Fewsnet for the given dates, status and regions
     """
     df = gpd.GeoDataFrame()
     for d in dates:
@@ -37,7 +46,17 @@ def shapefiles_to_df(path, status, dates, region, regionabb):
 
 def merge_admin2(df, path_admin, status, adm0c, adm1c, adm2c):
     """
-    Merge the geographic boundary information shapefile with the IPC shapefile.
+    Merge the geographic boundary information shapefile with the FewsNet dataframe.
+    Args:
+        df: DataFrame with the Fewsnet data and geometries
+        path_admin: path to file with admin(2) boundaries
+        status: type of FewsNet prediction: CS (current), ML1 (near-term projection) or ML2 (medium-term)
+        adm0c: column name of the admin0 level name, in boundary data
+        adm1c: column name of the admin1 level name, in boundary data
+        adm2c: column name of the admin2 level name, in boundary data
+
+    Returns:
+        overlap: dataframe with the regions per admin2 for each IPC level
     """
     admin2 = gpd.read_file(path_admin)
     admin2 = admin2[[adm0c, adm1c, adm2c, "geometry"]]
@@ -51,7 +70,19 @@ def merge_admin2(df, path_admin, status, adm0c, adm1c, adm2c):
 
 def return_max_cs(date, df, dfadcol, status, adm0c, adm1c, adm2c):
     """
-    Return the maximum IPC value in an admin2 district.
+    Return the IPC value that is assigned to the largest area (in m2) for the given Admin Level 2 region
+    Args:
+        date: string with the date of the FewsNet analysis
+        df: DataFrame that contains the geometrys per IPC level per Admin2 (output from merge_admin2)
+        dfadcol: one row of the df
+        status: type of FewsNet prediction: CS (current), ML1 (near-term projection) or ML2 (medium-term)
+        adm0c: column name of the admin0 level name, in boundary data
+        adm1c: column name of the admin1 level name, in boundary data
+        adm2c: column name of the admin2 level name, in boundary data
+
+
+    Returns:
+        row: row of the df which has the largest area within the given Admin Level 2 region (defined by dfadcol)
     """
     sub = df.loc[
         (df["date"] == date)
@@ -59,28 +90,12 @@ def return_max_cs(date, df, dfadcol, status, adm0c, adm1c, adm2c):
         & (df[adm2c] == dfadcol[adm2c])
     ]
     # if there are nan (=0) values we prefer to take the non-nan values, even if those represent a smaller area
+    # however if there are only nans (=0s) in an admin2 region, we do return one of those rows
     if len(sub[sub[status] != 0]) > 0:
         sub = sub[sub[status] != 0]
     mx = sub["area"].max()
     row = sub[["date", adm0c, adm1c, adm2c, status]].loc[sub["area"] == mx]
     return row
-
-
-# def return_max_cs(date, df, dfadcol, status, adm0c, adm1c, adm2c):
-#     """
-#     Return the maximum IPC value in an admin2 district.
-#     """
-#     sub = df.loc[
-#         (df["date"] == date)
-#         & (df[adm1c] == dfadcol[adm1c])
-#         & (df[adm2c] == dfadcol[adm2c])
-#     ]
-#     mx = sub[status].max()
-#     print(mx)
-#     print(date)
-#     row = sub[["date", adm0c, adm1c, adm2c, status]].loc[sub[status] == mx].iloc[0]
-#     # print(row)
-#     return row
 
 
 def gen_csml1m2(
@@ -94,6 +109,23 @@ def gen_csml1m2(
     region="east-africa",
     regionabb="EA",
 ):
+    """
+    Generate a DataFrame with the IPC level per Admin 2 Level, defined by the level that covers the largest area
+    The DataFrame includes all the dates given as input, and covers one type of classification given by status
+    Args:
+        ipc_path: path to the directory with the fewsnet data
+        bound_path: path to the file with the admin2 boundaries
+        status: type of FewsNet prediction: CS (current), ML1 (near-term projection) or ML2 (medium-term)
+        dates: list of dates for which FewsNet data should be included. The files of these dates have to be present in the directory (ipc_path)!
+        adm0c: column name of the admin0 level name, in boundary data
+        adm1c: column name of the admin1 level name, in boundary data
+        adm2c: column name of the admin2 level name, in boundary data
+        region: region that the fewsnet data covers, e.g. "east-africa"
+        regionabb: abbreviation of the region that the fewsnet data covers, e.g. "EA"
+
+    Returns:
+        new_df: DataFrame that contains one row per Admin2-date combination, which indicates the IPC level
+    """
     df_ipc = shapefiles_to_df(ipc_path, status, dates, region, regionabb)
     overlap = merge_admin2(df_ipc, bound_path, status, adm0c, adm1c, adm2c)
     # replace other values than 1-5 by 0 (these are 99,88,66 and indicate missing values, nature areas or lakes)
@@ -112,64 +144,64 @@ def gen_csml1m2(
 
 def main():
     """
-    Import the FEWSNET IPC maps, select the appropriate geographic boundary, select the max IPC category.
-    Saves a CSV to file and returns the dataframe.
+    Set all variables, run the function for the different forecasts, and save as csv
     """
 
-    DATES = [
-        "200907",
-        "200910",
-        "201001",
-        "201004",
-        "201007",
-        "201010",
-        "201101",
-        "201104",
-        "201107",
-        "201110",
-        "201201",
-        "201204",
-        # "201207", #no data for Malawi
-        "201210",
-        "201301",
-        "201304",
-        "201307",
-        "201310",
-        "201401",
-        "201404",
-        "201407",
-        "201410",
-        "201501",
-        "201504",
-        "201507",
-        "201510",
-        "201602",
-        "201606",
-        "201610",
-        "201706",
-        "201710",
-        "201802",
-        "201806",
-        "201810",
-        "201812",
-        "201902",
-        "201906",
-        "201910",
-        # "202002",
-        # "202003",
-        # "202004",
-        # "202005",
-        # "202006",
-        # "202007",
-        # "202008"
-    ]
+    DATES = ["200907"]
+    #     [
+    #     "200907",
+    #     "200910",
+    #     "201001",
+    #     "201004",
+    #     "201007",
+    #     "201010",
+    #     "201101",
+    #     "201104",
+    #     "201107",
+    #     "201110",
+    #     "201201",
+    #     "201204",
+    #     "201207", #no data for Malawi
+    #     "201210",
+    #     "201301",
+    #     "201304",
+    #     "201307",
+    #     "201310",
+    #     "201401",
+    #     "201404",
+    #     "201407",
+    #     "201410",
+    #     "201501",
+    #     "201504",
+    #     "201507",
+    #     "201510",
+    #     "201602",
+    #     "201606",
+    #     "201610",
+    #     "201706",
+    #     "201710",
+    #     "201802",
+    #     "201806",
+    #     "201810",
+    #     # "201812", #no data for ethiopia
+    #     "201902",
+    #     "201906",
+    #     "201910",
+    #     "202002",
+    #     # "202003",
+    #     # "202004",
+    #     # "202005",
+    #     "202006",
+    #     # "202007",
+    #     "202008"
+    # ]
     STATUS_LIST = ["CS", "ML1", "ML2"]
-    COUNTRY = "malawi"  # "ethiopia"
-    REGION = "southern-africa"  # "east-africa"
-    REGIONCODE = "SA"  # "EA"
+    COUNTRY = "ethiopia"  # "malawi"  #
+    REGION = "east-africa"  # "southern-africa"  #
+    REGIONCODE = "EA"  # "SA"  #
     PATH = "Data/FewsNetRaw/"
     PATH_RESULT = f"{COUNTRY}/Data/FewsNetAdmin2/"  # OldShp/"
-    ADMIN2_SHP = "mwi_adm_nso_20181016_shp/mwi_admbnda_adm2_nso_20181016.shp"  # "ET_Admin2_OCHA_2019/eth_admbnda_adm2_csa_bofed_20190827.shp" # 'ET_Admin2_2014/ET_Admin2_2014.shp'
+    ADMIN2_SHP = "ET_Admin2_OCHA_2019/eth_admbnda_adm2_csa_bofed_20190827.shp"  # "mwi_adm_nso_20181016_shp/mwi_admbnda_adm2_nso_20181016.shp"  #  # 'ET_Admin2_2014/ET_Admin2_2014.shp'
     ADMIN2_PATH = f"{COUNTRY}/Data/{ADMIN2_SHP}"
 
     for STATUS in STATUS_LIST:
